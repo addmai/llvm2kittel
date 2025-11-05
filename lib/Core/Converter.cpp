6870b487a77a0b819122dcfd465762a4fde67dd5
iwancof <rock.iwancof@gmail.com>
2025-11-06 03:36:53 +0900

    treat uninitialized global array as 0 initialized

--- lib/Core/Converter.cpp
+++ lib/Core/Converter.cpp
@@ -307,14 +307,12 @@ void Converter::phase1(
         } else {
           // Define an array -> Globally -> One-dimensional -> 1
           std::string arrayName = "v" + varName;
-          // if (globalVar.hasInitializer() &&
-          // llvm::isa<llvm::ConstantAggregateZero>(const_cast<llvm::Constant
-          // *>(globalVar.getInitializer()))) {
-          //     globalInsts.push_back(arrayName + " := const_array(0);");
-          // }
-          // else {
-          globalInsts.push_back("v" + varName + " := nondet();");
-          //}
+          if (GV.hasInitializer() &&
+              llvm::isa<llvm::ConstantAggregateZero>(const_cast<llvm::Constant *>(GV.getInitializer()))) {
+            globalInsts.push_back(arrayName + " := const_array(0);");
+          } else {
+            globalInsts.push_back("v" + varName + " := nondet();");
+          }
           if (std::find(oneDimArrs.begin(), oneDimArrs.end(), arrayName) ==
               oneDimArrs.end()) {
             oneDimArrs.push_back(arrayName);
